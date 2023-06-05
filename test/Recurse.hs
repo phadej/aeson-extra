48c72c9ed9c5637c975b2a0d4c7f98ec5a48a0df
Phil de Joux <philderbeast@gmail.com>
2023-06-05 12:13:10 -0400

    Add sorting keys and combined examples.

--- test/Recurse.hs
+++ test/Recurse.hs
@@ -19,15 +19,43 @@ import qualified Data.HashMap.Strict as HM
 import qualified Data.Vector as V
 
 empty :: Value
-empty = $(mkValue' "{}")
+empty = $(mkValue "{}")
 
 sortedInts :: Value
-sortedInts = $(mkValue' "{\"ints\": [1,2,3]}")
+sortedInts = $(mkValue' "{'ints': [1,2,3]}")
+
+sortedKeys :: Value
+sortedKeys = $(mkValue' "{'a': 'x', 'b': 'y', 'c': 'z'}")
+
+combined :: Value
+combined = $(mkValue' "{'b': 'y', 'c': [1,2,3]}")
+
+nested :: Value
+nested = $(mkValue' "{'abc': {'a': 'x', 'b': 'y', 'c': 'z'}, 'ints': [1,2,3], 'value': null}")
 
 recurseTests :: TestTree
 recurseTests = testGroup "Recurse examples"
-  [ testCase "strip nulls" $ stripNulls $(mkValue' "{\"value\": null}") @?= empty
-  , testCase "sort arrays" $ sortArrays $(mkValue' "{\"ints\": [2,1,3]}") @?= sortedInts
+  [ testCase "strip nulls" $ stripNulls $(mkValue' "{'value': null}") @?= empty
+  , testCase "sort arrays" $ sortArrays $(mkValue' "{'ints': [2,1,3]}") @?= sortedInts
+  , testCase "sort keys" $ sortKeys $(mkValue' "{'b': 'y', 'c': 'z', 'a': 'x'}") @?= sortedKeys
+
+  , testCase "strip nulls . sort arrays . sort keys" $ let f = stripNulls . sortArrays . sortKeys in
+        f $(mkValue' "{'b': 'y', 'c': [2,3,1], 'a': null }") @?= combined
+  , testCase "strip nulls . sort keys . sort arrays" $ let f = stripNulls . sortKeys . sortArrays in
+        f $(mkValue' "{'b': 'y', 'c': [2,3,1], 'a': null }") @?= combined
+
+  , testCase "sort keys . strip nulls . sort arrays" $ let f = sortKeys . stripNulls . sortArrays in
+        f $(mkValue' "{'b': 'y', 'c': [2,3,1], 'a': null }") @?= combined
+  , testCase "sort keys . sort arrays . strip nulls" $ let f = sortKeys . sortArrays . stripNulls in
+        f $(mkValue' "{'b': 'y', 'c': [2,3,1], 'a': null }") @?= combined
+
+  , testCase "sort arrays . sort keys . strip nulls" $ let f = sortArrays . sortKeys . stripNulls in
+        f $(mkValue' "{'b': 'y', 'c': [2,3,1], 'a': null }") @?= combined
+  , testCase "sort arrays . strip nulls . sort keys" $ let f = sortArrays . stripNulls . sortKeys in
+        f $(mkValue' "{'b': 'y', 'c': [2,3,1], 'a': null }") @?= combined
+
+  , testCase "nested sorting" $ let f = sortArrays . sortKeys in
+        f $(mkValue' "{'value': null, 'ints': [2,3,1], 'abc': {'b': 'y', 'c': 'z', 'a': 'x'}}") @?= nested
   ]
 
 stripNulls :: Value -> Value
@@ -38,4 +66,9 @@ stripNulls = cata (embed . f) where
 sortArrays :: Value -> Value
 sortArrays = cata (embed . f) where
   f (ArrayF xs) = ArrayF (V.fromList . sort $ V.toList xs)
+  f x = x
+
+sortKeys:: Value -> Value
+sortKeys = cata (embed . f) where
+  f (ObjectF a) = ObjectF (HM.fromList . sort $ HM.toList a)
   f x = x
