9e6d0db48784d659e2675952934a23b458c448e5
Phil de Joux <philderbeast@gmail.com>
2023-06-05 11:53:53 -0400

    Add a test of sorting arrays.

--- test/Recurse.hs
+++ test/Recurse.hs
@@ -10,21 +10,32 @@ import Prelude.Compat
 import Data.Aeson
 import Data.Aeson.Extra
 import Data.Functor.Foldable (cata, embed)
+import Data.List (sort)
 
 import Test.QuickCheck.Instances ()
 import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Data.HashMap.Strict as HM
+import qualified Data.Vector as V
 
 empty :: Value
 empty = $(mkValue' "{}")
 
+sortedInts :: Value
+sortedInts = $(mkValue' "{\"ints\": [1,2,3]}")
+
 recurseTests :: TestTree
 recurseTests = testGroup "Recurse examples"
   [ testCase "strip nulls" $ stripNulls $(mkValue' "{\"value\": null}") @?= empty
+  , testCase "sort arrays" $ sortArrays $(mkValue' "{\"ints\": [2,1,3]}") @?= sortedInts
   ]
 
 stripNulls :: Value -> Value
 stripNulls = cata (embed . f) where
   f (ObjectF a) = ObjectF $ HM.filter (/= Null) a
+  f x = x
+
+sortArrays :: Value -> Value
+sortArrays = cata (embed . f) where
+  f (ArrayF xs) = ArrayF (V.fromList . sort $ V.toList xs)
   f x = x
